d20bf9f3777f2ac42a41294cbe06af3a194bd3a1
Rei Zhang <mauoser@outlook.com>
2025-11-22 04:40:24 -0500

    updated app with role-based routing

--- src/App.js
+++ src/App.js
@@ -5,31 +5,64 @@ import {
   Route,
   Navigate,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Navbar from "./App/Navbar/Navbar";
+import ProtectedRoute from "./components/ProtectedRoute";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import JobListings from "./pages/JobListings";
 import Contact from "./pages/Contact";
 import CompanyShowcase from "./pages/CompanyShowcase";
 import Login from "./pages/Login";
-import { AuthProvider } from "./services/AuthContext";
+import AdminEmployees from "./pages/AdminEmployees";
+import AdminAddJob from "./pages/AdminAddJob";
 
 function App() {
+  const user = useSelector((state) => state.auth.user);
+
   return (
-    <AuthProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/jobs" element={<JobListings />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/companies" element={<CompanyShowcase />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Navbar />
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/companies" element={<CompanyShowcase />} />
+        <Route path="/login" element={<Login />} />
+
+        {/* Employee Routes (Protected) */}
+        <Route
+          path="/jobs"
+          element={
+            <ProtectedRoute requiredRole="employee">
+              <JobListings />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Admin Routes (Protected) */}
+        <Route
+          path="/admin/employees"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <AdminEmployees />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/admin/add-job"
+          element={
+            <ProtectedRoute requiredRole="admin">
+              <AdminAddJob />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Catch all */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
